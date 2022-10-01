23cd3c774cb0a86c7fe60bf14d2530b3f9c0b4fb
Akilah Powell <atpjun@gmail.com>
2022-10-01 19:36:54 -0400

    new changes

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-// grab DOM elements
+// DOM - document object model
+// built into your browser -- its a JS representation of your HTML
 
-// set event listeners
+// DOM GETTER METHODS
+// getElementById
+const nameDisplay = document.getElementById('name');
+const pronounDisplay= document.getElementById('pronoun');
+
+// DOM SETTER METHODS
+// textContent
+nameDisplay.textContent = 'Akilah';
+nameDisplay.classList.add('yellow');
+
+const updateBtn = document.getElementById('update-btn');
+updateBtn.addEventListener('click', () => {
+    
+    
+    const nameInput = document.getElementById('name-input');
+
+    nameDisplay.textContent = nameInput.value;
+});
+
+const updatebutton = document.getElementById('update-button')
+updatebutton.addEventListener('click', () => {
+    console.log('Updating pronouns');
+    
+    const pronounInput = document.getElementById('pronoun-input');
+
+    pronounDisplay.textContent = pronounInput.value;
+
+})
